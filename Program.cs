10ea37fb2ffb7bc177edc14a6da6e64509d8affb
OrielGelberg <g9093587@gmail.com>
2025-05-27 10:01:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos27._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
